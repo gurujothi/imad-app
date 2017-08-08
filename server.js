cb958d3c187327d3489b14c59acb941ad25e370d
gurujothi <gurumoorthy1994@gmail.com>
2017-08-08 12:36:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ return htmlTemplate;
 }
 app.get('/:articleName', function (req, res) {
     var articleName=req.params.articleName;
-  res.send(createTemplate(articles(articleName)));
+  res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
