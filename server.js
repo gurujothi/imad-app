30ca68188f661a18d3ef05646e52b159e363c529
gurujothi <gurumoorthy1994@gmail.com>
2017-08-08 13:50:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,8 +80,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-two', function (req, res) {
-  res.send(createTemplate(articleTwo));
+app.get('/article-three', function (req, res) {
+  res.send(createTemplate(articleThree));
 });
 
 app.get('/ui/style.css', function (req, res) {
