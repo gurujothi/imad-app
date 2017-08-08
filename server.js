fa504c8548e162c876a3aa91333461a19bc7332f
gurujothi <gurumoorthy1994@gmail.com>
2017-08-08 15:15:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,30 +5,31 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articleOne={
-        title: 'Article 1 - Guru',
-        heading: 'MY FIRST ARTICLE ABOUT ME',
-        date:'Jan 16, 2015',
-        content:`<p>     Hi! Welcome to my First Article. I am Gurumoorthy. Completed B.E. </p>
-            <p>Greetings from NovelVista learning Solutions! Thanks for showing interest in NovelVista Learning Solutions!!
-It was nice to talk to you today. Please find below details for ITIL Foundation Certification.
- 
-We are the global and leading Accredited Training Organization (ATO) for ITIL, PRINCE2, Lean Six Sigma, Agile Scrum Master, ISO 20000/27000, CSM, MSP, PMP, Cloud Computing, etc. !!!
-ITIL® (IT Infrastructure Library®) is the most widely established approach to IT Service Management. It provides a set of best practices for identifying, planning, delivering and supporting IT services to businesses and can be applied to nearly all organizations.</p>`
-        };
-var articleTwo={
-        title: 'Article 2 - Guru',
-        heading: 'MY SECOND ARTICLE ABOUT MY JOB',
-        date:'August 15, 2015',
-        content:`Hi! Welcome to my Second Article. I am Gurumoorthy. working on DXC Technology. Healthways Account.`
-        };
-var articleThree={
-        title: 'Article 3 - Guru',
-        heading: 'MY THIRD ARTICLE ABOUT LIFE',
-        date:'Nov 16,2016',
-        content:`Hi! Welcome to my Third Article. I am Gurumoorthy. A Searcher of science.`
-        };
-
+var articles=   {
+     'article-one':{
+            title: 'Article 1 - Guru',
+            heading: 'MY FIRST ARTICLE ABOUT ME',
+            date:'Jan 16, 2015',
+            content:`<p>     Hi! Welcome to my First Article. I am Gurumoorthy. Completed B.E. </p>
+                <p>Greetings from NovelVista learning Solutions! Thanks for showing interest in NovelVista Learning Solutions!!
+    It was nice to talk to you today. Please find below details for ITIL Foundation Certification.
+     
+    We are the global and leading Accredited Training Organization (ATO) for ITIL, PRINCE2, Lean Six Sigma, Agile Scrum Master, ISO 20000/27000, CSM, MSP, PMP, Cloud Computing, etc. !!!
+    ITIL® (IT Infrastructure Library®) is the most widely established approach to IT Service Management. It provides a set of best practices for identifying, planning, delivering and supporting IT services to businesses and can be applied to nearly all organizations.</p>`
+            },
+     'article-two':{
+            title: 'Article 2 - Guru',
+            heading: 'MY SECOND ARTICLE ABOUT MY JOB',
+            date:'August 15, 2015',
+            content:`Hi! Welcome to my Second Article. I am Gurumoorthy. working on DXC Technology. Healthways Account.`
+            },
+     'article-three':{
+            title: 'Article 3 - Guru',
+            heading: 'MY THIRD ARTICLE ABOUT LIFE',
+            date:'Nov 16,2016',
+            content:`Hi! Welcome to my Third Article. I am Gurumoorthy. A Searcher of science.`
+            }
+};
 function createTemplate (data)
 {
 var title = data.title;
@@ -80,8 +81,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-three', function (req, res) {
-  res.send(createTemplate(articleThree));
+app.get('/:articleName', function (req, res) {
+  var articleName = res.params.articleName;
+  res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
