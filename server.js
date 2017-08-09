7560f2769c40cab7d859e0898b874793cb723c2a
gurujothi <gurumoorthy1994@gmail.com>
2017-08-09 12:09:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ app.get('/:articleName', function (req, res) {
   res.send(createTemplate(articles[articleName]));
 });
 app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+  alert('This is SERVER FILE');
 });
 
 app.get('/ui/style.css', function (req, res) {
