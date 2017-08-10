a3653ac14e38e60cc43a9dd3e8d68e7d98a24d6a
gurujothi <gurumoorthy1994@gmail.com>
2017-08-10 15:57:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,10 +84,6 @@ app.get('/counter', function (req,res) {
    counter = counter + 1;
    res.send(counter.toString()); 
 });
-app.get('/:articleName', function (req, res) {
-  var articleName = req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
-});
 
 var names=[];
 app.get('/submit-name', function (req, res) {
@@ -95,6 +91,13 @@ app.get('/submit-name', function (req, res) {
     names.push(name);
     res.send(JSON.stringify(names));
 });
+
+
+app.get('/:articleName', function (req, res) {
+  var articleName = req.params.articleName;
+  res.send(createTemplate(articles[articleName]));
+});
+
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
