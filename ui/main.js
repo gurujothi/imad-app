7d8e0112cc275e414a9858e9a81d8a53a61dab4e
gurujothi <gurumoorthy1994@gmail.com>
2017-08-10 15:28:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,12 +30,12 @@ submit.onclick = function() {
     var names = ['name1','name2','name3','name4'];
     var list = '';
     for (var i = 0; i<names.length; i++) {
-        list += '<li>'+names[i]+'</li>';
+        list += '<li>' + names[i] + '</li>';
         
         
     }
     
     var ui = document.getElementById('namelist');
-    ui.InnerHTML = list;
+    ui.innerHTML = list;
     
 };
