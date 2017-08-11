cda872900dc0213754c4bb07d5797bc89ee10c1f
gurujothi <gurumoorthy1994@gmail.com>
2017-08-11 12:31:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -53,3 +53,35 @@ submit.onclick = function() {
     
     
 };
+
+var contentInput = document.getElementById('comments');
+
+var submitbtn = document.getElementById('submitcontent');
+submitbtn.onclick = function() {
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function() {
+        
+        if(request.readyState === XMLHttpRequest.DONE){
+            
+            if(request.status === 200){
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for (var i = 0; i<names.length; i++) {
+                    list += '<li>' + names[i] + '</li>';
+                }
+                
+                var ui = document.getElementById('nameList');
+                ui.innerHTML = list;
+                            
+                
+            }
+        }
+    };
+    var Name = contentInput.value;
+    request.open('GET','http://gurumoorthy1994.imad.hasura-app.io/submit-name?name=' + Name, true);
+    request.send(null);
+    
+    
+};
