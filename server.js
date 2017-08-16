0a57ff3c777ecf61396f524c3fdb98dca64e7394
gurujothi <gurumoorthy1994@gmail.com>
2017-08-16 16:17:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -152,7 +152,26 @@ app.get('/:articleName', function (req, res) {
   
 });
 
-
+app.get('/articles/:articleName', function (req, res) {
+  
+  pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function(err,result){
+     if(err){
+         res.status(500).send(err.toString());
+     } 
+     else{
+         if(result.rows.lenght === 0){
+             res.status(404).send("Article not Found");
+             
+         }
+         else{
+              var articleData = result.rows[0];
+              res.send(createTemplate(articleData));
+                         
+         }
+     }
+  });
+  
+});
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
