c790dd4aadb135604474afa3fe8781928b0a2722
gurujothi <gurumoorthy1994@gmail.com>
2017-08-16 15:47:53 +0530

    [imad-console] Updates ui/mainarticle.js

--- ui/mainarticle.js
+++ ui/mainarticle.js
@@ -1,31 +1,7 @@
-var button = document.getElementById('counter');
+var contentInput = document.getElementById('comments');
 
-button.onclick = function() {
-    
-    var request = new XMLHttpRequest();
-    
-    request.onreadystatechange = function() {
-        
-        if(request.readyState === XMLHttpRequest.DONE){
-            
-            if(request.status === 200){
-                
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-                
-            }
-        }
-    };
-    request.open('GET','http://gurumoorthy1994.imad.hasura-app.io/counter', true);
-    request.send(null);
-    
-};
-
-var nameInput = document.getElementById('name');
-
-var submit = document.getElementById('submit_btn');
-submit.onclick = function() {
+var submitbtn = document.getElementById('submitcontent');
+submitbtn.onclick = function() {
     var request = new XMLHttpRequest();
     
     request.onreadystatechange = function() {
@@ -40,17 +16,18 @@ submit.onclick = function() {
                     list += '<li>' + names[i] + '</li>';
                 }
                 
-                var ui = document.getElementById('namelist');
+                var ui = document.getElementById('nameList');
                 ui.innerHTML = list;
                             
                 
             }
         }
     };
-    var Name = nameInput.value;
-    request.open('GET','http://gurumoorthy1994.imad.hasura-app.io/submit-name?name=' + Name, true);
+    var Name = contentInput.value;
+    
+    request.open('GET','http://gurumoorthy1994.imad.hasura-app.io/article1Comment?content=' + Name, true);
+    
     request.send(null);
     
     
 };
-
