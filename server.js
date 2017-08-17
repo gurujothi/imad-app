949cb80b194053440d09530bd08cd149b104f3cc
gurujothi <gurumoorthy1994@gmail.com>
2017-08-17 11:42:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -154,12 +154,12 @@ app.get('/:articleName', function (req, res) {
 
 app.get('/articles/:articleName', function (req, res) {
   
-  pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function(err,result){
+  pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function(err,result){
      if(err){
          res.status(500).send(err.toString());
      } 
      else{
-         if(result.rows.lenght === 0){
+         if(result.rows.length === 0){
              res.status(404).send("Article not Found");
              
          }
