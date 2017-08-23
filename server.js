bc88b9b7bf4d1aa351d004f02d10b02877e17ce6
gurujothi <gurumoorthy1994@gmail.com>
2017-08-23 15:41:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -145,7 +145,7 @@ app.post('/login', function(req,res){
    
    pool.query(`SELECT * FROM "user" WHERE username = $1`,[username], function(err,result){
      if(err){
-         res.status(500).send(err.toString());
+         res.status(500).send('hi');
             } 
      else{
          if(results.rows.length === 0){
