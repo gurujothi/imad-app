0ab0a43973f77a39d786b7cba9a4f7bff441a253
gurujothi <gurumoorthy1994@gmail.com>
2017-08-23 12:13:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 
 var config = {
     user: 'gurumoorthy1994',
@@ -108,6 +109,14 @@ return htmlTemplate;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+function hashFunction(password,salt){
+    var hashed = crypto.pbkdf2Sync(password, 'salt', 10000, 512, 'sha512');
+}
+app.get('/hash/:password', function (req, res) {
+  var hashedString = hashFunction(req.params.password,'This_is_some_random_string')
+  res.send(hashedString);
 });
 
 var pool = new Pool(config);
