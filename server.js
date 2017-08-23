1b2cdc55e34a02ff0325b3d96c8d78e98828d3e5
gurujothi <gurumoorthy1994@gmail.com>
2017-08-23 13:44:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -113,12 +113,14 @@ app.get('/', function (req, res) {
 
 function hashFunction(password,salt){
     var hashed = crypto.pbkdf2Sync(password, 'salt', 10000, 512, 'sha512');
-    return hashed.toString('hex');
+    return ["pbkdf2","10000",salt,hashed.toString('hex')].join('$');
 }
 app.get('/hash/:password', function (req, res) {
   var hashedString = hashFunction(req.params.password,'This_is_some_random_string')
   res.send(hashedString);
 });
+
+
 
 var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
