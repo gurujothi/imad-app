f3802e8ec3248bff76fe475e669bfb064666b2e6
gurujothi <gurumoorthy1994@gmail.com>
2017-08-24 11:52:26 +0530

    [imad-console] Updates ui/mainarticle.js

--- ui/mainarticle.js
+++ ui/mainarticle.js
@@ -31,3 +31,27 @@ submitbtn.onclick = function() {
     
     
 };
+
+var button = document.getElementById('counter');
+
+button.onclick = function() {
+    
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function() {
+        
+        if(request.readyState === XMLHttpRequest.DONE){
+            
+            if(request.status === 200){
+                
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+                
+            }
+        }
+    };
+    request.open('GET','http://gurumoorthy1994.imad.hasura-app.io/counter', true);
+    request.send(null);
+    
+};
